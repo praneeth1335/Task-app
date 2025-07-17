e581d381f6c25ef56197d7efe4fb8aa56af5c868
Praneeth <bspraneeth05@gmail.com>
2025-07-17 11:09:51 +0530

    tust proxy

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+// In index.js
+app.set("trust proxy", 1);
 
 // Session configuration
 app.use(
